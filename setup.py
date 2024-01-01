a94dab28e5cfa9f88a03cdd32e1d90f90109f3c9
ta-ke-inf <hirotake.stu@gmail.com>
2024-01-02 07:42:15 +0900

    update setup.py

--- setup.py
+++ setup.py
@@ -9,7 +9,16 @@ print("Found packages:", find_packages())
 
 setup(
     name="qdvis",
+    description="qdvis: Quick data visualizer.",
+    author="Takeguchi",
+    author_email="hirotake.stu@gmail.com",
+    maintainer="Takeguchi",
+    maintainer_email="hirotake.stu@gmail.com",
+    url="https://github.com/ta-ke-inf/qdvis/tree/main",
+    download_url="https://github.com/ta-ke-inf/qdvis/tree/main",
+    license="MIT",
     version="0.1.0",
+    keywords="visualization ml",
     packages=find_packages(),
     include_package_data=True,
     install_requires=requirements_from_file("requirements.txt"),
